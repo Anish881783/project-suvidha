b40d90d87ac72beba7e57c2dbf46a2ced917785b
Anish Patel <anishnick11@gmail.com>
2023-01-16 20:41:54 +0530

    first commit

---
+++ css/Untitled-1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+void print(int n){
+    if(n==0){
+         cout<< "0";
+    }
+    else if(n==1){
+        cout<<"1";
+    }
+    print(n-1);
+    cout<<n;
+}
+int main(){
+    int n;
+    cin>>n;
+    print(n);
+}
